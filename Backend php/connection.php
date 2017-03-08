af60aae0d60cb33b287ed0c8e07266160e6643b1
Dylan <dylanliu95@msn.com>
2017-03-07 23:12:32 -0500

    Needed to connect to database

---
+++ Backend php/connection.php
@@ -0,0 +1,24 @@
+<?php
+
+	// returns a DB connection handle. Otherwise returns null.
+	function dbConnect() {
+		// this will avoid mysql_connect() deprecation error.
+		error_reporting( ~E_DEPRECATED & ~E_NOTICE );
+	 
+		define('DBHOST', '127.0.0.1');
+		define('DBUSER', 'root');
+		define('DBPASS', 'devcpp');
+		define('DBNAME', 'cisc498');
+	 
+		$conn = mysqli_connect(DBHOST,DBUSER,DBPASS);
+		if ( !$conn ) {
+			die("Connection failed!!!");
+		}
+		$dbcon = mysqli_select_db($conn, DBNAME); 
+		if ( !$dbcon ) {
+			die("Database Connection failed!!!");
+		}
+		return $conn;
+	}
+
+?>
